47619ae16edd48cec8eb77d9c017e6314d8129ec
exequiel <exeluna666@gmail.com>
2024-09-11 14:57:16 -0300

    se actualizo el header de las paginas

--- JS/header.js
+++ JS/header.js
@@ -1,18 +1,14 @@
 const header = document.getElementById('header');
 
-header.innerHTML =`
-<header>
-        <nav class="navbar navbar-expand-lg">
+header.innerHTML =`<header>
+        <nav class="navbar navbar-expand-lg" style="background: #8D493A;">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">LOGO</a>
+                <a href="menu.html"><img src="../IMG/logo.webp" alt="logotipo" style="height: 50px; width: 50px; border-radius: 4px;"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">Menú</a>
-                        </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Link</a>
                         </li>
