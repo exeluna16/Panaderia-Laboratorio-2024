8d06c0a26531553ce402a2ce22617edb474c2801
exequiel <exeluna666@gmail.com>
2024-10-21 13:51:39 -0300

    se arreglo la plantilla base.html

--- panaderia/panaderia/settings.py
+++ panaderia/panaderia/settings.py
@@ -10,13 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
+#from pathlib import Path
 import os
 
+#from django_extensions.settings import BASE_DIR
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -129,7 +130,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
-
+print(BASE_DIR)
+print(STATIC_URL)
+print(STATICFILES_DIRS)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
