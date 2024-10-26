28478f818f4f62d4f5cff1a311e54cdb8c5fd250
exequiel <exeluna666@gmail.com>
2024-10-26 13:57:11 -0300

    se creo el modelo de pedido

--- panaderia/apps/pedido/models.py
+++ panaderia/apps/pedido/models.py
@@ -1,3 +1,10 @@
+from idlelib.tree import TreeItem
+from ..proveedores.models import Proveedor
 from django.db import models
 
 # Create your models here.
+class Pedido(models.Model):
+    numero = models.DecimalField()
+    fecha_realizado = models.DateTimeField(auto_now_add=True)
+    observacion = models.CharField(max_length=150,null=True)
+    cuit_proveedor = models.ForeignKey(Proveedor,on_delete=models.SET_NULL,null=True,related_name='Proveedor')
