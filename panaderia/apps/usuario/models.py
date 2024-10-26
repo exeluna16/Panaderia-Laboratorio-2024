998a4686ef02ce0829d58d8cad25f830db1acffd
exequiel <exeluna666@gmail.com>
2024-10-26 03:13:16 -0300

    se modifico el modelo de persona

--- panaderia/apps/usuario/models.py
+++ panaderia/apps/usuario/models.py
@@ -10,9 +10,14 @@ class Usuario(models.Model):
 
 #Como existen muchas personas en el sistema se opto por una clase abstracta
 class Persona(models.Model):
+    TIPO_PERSONA=[
+        ('FISICA','FISICA'),
+        ('JURIDICA','JURIDICA')
+    ]
+
     nombre = models.CharField(max_length=150)
     mail = models.EmailField(max_length=150)
-    tipo_persona = models.CharField(max_length=20)
+    tipo_persona = models.CharField(max_length=20,choices=TIPO_PERSONA)
     estado = models.BooleanField(default=True)
     calle=models.CharField(max_length=150)
     localidad = models.CharField(max_length=150)
