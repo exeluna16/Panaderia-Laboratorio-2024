84b3e722bbde6107713a1cd9f2bb0a703581c709
exequiel <exeluna666@gmail.com>
2024-10-28 22:45:00 -0300

    se agregaron permisos al registro de clientes mayoristas

--- panaderia/apps/cliente_mayorista/views.py
+++ panaderia/apps/cliente_mayorista/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .forms import ClienteMayoristaForm
+from django.contrib.auth.decorators import login_required #controla el usaurio logueado
 # Create your views here.
+#controla que el usuario este logueado, si no lo esta lo redireciona al login
+@login_required(login_url='usuario:login')
 def agregar_cliente_mayorista(request):
     form = ClienteMayoristaForm()
     if request.method=='POST':
