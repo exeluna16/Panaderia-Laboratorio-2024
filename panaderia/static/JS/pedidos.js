5cfb691a9827232fb5211c88460f91ff5cb74f41
exequiel <exeluna666@gmail.com>
2024-11-01 00:52:35 -0300

    se actualizo el script de los pedidos

--- panaderia/static/JS/pedidos.js
+++ panaderia/static/JS/pedidos.js
@@ -1,12 +1,50 @@
-document.addEventListener('DOMContentLoaded',function(){
+let proveedores = [];
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    fetch('../proveedores/ver_proveedores')//se envia la petición
+    .then(respuesta => respuesta.json())//transformo la respuesta en un JSON
+    .then(datos =>{
+        proveedores = datos
+    })
+    .catch(error=>console.error('No se pudo traer a los proveedores de la BD',error));
+    
+
+
     const fecha_actual = new Date();
     //agrego la fecha actual
-    const fecha_formateada = `${fecha_actual.getFullYear()}-${fecha_actual.getMonth()+1}-${fecha_actual.getDate()}`;
+    //.padStart(2, '0') se asegura de que los dias o meses tengan el 0 delante si son menores a 10.
+    const fecha_formateada = `${fecha_actual.getFullYear()}-${String(fecha_actual.getMonth() + 1).padStart(2, '0')}-${String(fecha_actual.getDate()).padStart(2, '0')}`;
     document.getElementById('fecha_pedido').value = fecha_formateada;
     //desabilito el campo para que no se pueda cambiar
     document.getElementById('fecha_pedido').disabled = true;
 
+    
 });
 
+
+function buscarProveedor(){
+    const consulta = document.getElementById('buscador-proveedor').value.toLowerCase();
+    const lista_proveedores = document.getElementById('lista-proveedores');
+    lista_proveedores.innerHTML = '';
+
+    const resultados = proveedores.filter(p =>p.nombre.toLowerCase().includes(consulta));
+
+    resultados.forEach(proveedor=>{
+        const li = document.createElement('li');
+        li.classList.add('list-group-item','list-group-item-action');
+        li.textContent = proveedor.nombre;
+        li.onclick = () => seleccionarProveedor(proveedor);
+        lista_proveedores.appendChild(li);
+    });
+}
+
+function seleccionarProveedor(proveedor){
+    document.getElementById('proveedor-elegido').value = proveedor.nombre;
+    //a continuacion se cierra el modal de Bootstrap
+    //para eso se necesita instanciar la clase Modal y llamar el metodo .hide()    
+    const modal = bootstrap.Modal.getInstance(document.getElementById('staticBackdrop'));
+    modal.hide();
+}
 
 
