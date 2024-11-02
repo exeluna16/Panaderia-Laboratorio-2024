9033132ee7683b97a0df5a55597c2b9fce9d17a3
exequiel <exeluna666@gmail.com>
2024-11-02 04:13:35 -0300

    se creo el formulario para los items del pedido

--- panaderia/apps/pedido/forms.py
+++ panaderia/apps/pedido/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Pedido
+from django.forms import inlineformset_factory
+from .models import Pedido, ItemPedido
 
 class PedidoForm(forms.ModelForm):
     class Meta:
@@ -9,3 +10,24 @@ class PedidoForm(forms.ModelForm):
             'observacion':forms.TextInput(attrs={'class':'form-control'}),
             'cuit_proveedor':forms.HiddenInput(),
         }
+
+
+class ItemPedidoForm(forms.ModelForm):
+    class Meta:
+        model = ItemPedido
+        fields = ['pedido','insumo','cantidad']
+        widgets = {
+            'cantidad':forms.NumberInput(attrs={'class':'form-control cantidad-insumo'}),
+            'pedido': forms.HiddenInput(),
+            'insumo': forms.HiddenInput(attrs={'class':'insumo-seleccionado'}),
+        }
+
+
+#formulario de tipo formset, crea varias instancias de un formulario
+ItemPedidoFormSet = inlineformset_factory (
+    Pedido, #modelo principal
+    ItemPedido, #modelo relacionado
+    form = ItemPedidoForm,
+    extra = 1,
+    can_delete=True,
+)
