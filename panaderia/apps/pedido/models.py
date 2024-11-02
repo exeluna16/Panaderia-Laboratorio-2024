bfff926b931096febbf51c8e0841b8a67f4e3ad7
exequiel <exeluna666@gmail.com>
2024-11-02 03:50:08 -0300

    se creo el modelo de item de pedido

--- panaderia/apps/pedido/models.py
+++ panaderia/apps/pedido/models.py
@@ -1,4 +1,5 @@
 from ..proveedores.models import Proveedor
+from ..inventario.models import Insumo
 from django.db import models
 
 # Create your models here.
@@ -7,3 +8,11 @@ class Pedido(models.Model):
     fecha_realizado = models.DateTimeField(auto_now_add=True)
     observacion = models.CharField(max_length=150,null=True)
     cuit_proveedor = models.ForeignKey(Proveedor,on_delete=models.SET_NULL,null=True,related_name='Proveedor')
+
+
+class ItemPedido(models.Model):
+    pedido_id = models.ForeignKey(Pedido,on_delete = models.SET_NULL,null= True)
+    insumo_id = models.ForeignKey(Insumo,on_delete=models.SET_NULL,null = True)
+    cantidad = models.IntegerField(null=False,blank=False)
+
+
