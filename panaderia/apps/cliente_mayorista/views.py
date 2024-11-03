89f6f619027e741c07b7d5b11661aeb5524db566
Marcos Acosta <acostamarlih9@gmail.com>
2024-11-03 01:42:03 -0300

    corregida alta, modificación y baja de clientes mayoristas

--- panaderia/apps/cliente_mayorista/views.py
+++ panaderia/apps/cliente_mayorista/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,get_object_or_404, redirect
 from .forms import ClienteMayoristaForm,ModificarClienteMayoristaForm
 from django.contrib.auth.decorators import login_required #controla el usaurio logueado
 from .models import ClienteMayorista
+from django.contrib import messages
 # Create your views here.
 #controla que el usuario este logueado, si no lo esta lo redireciona al login
 @login_required(login_url='usuario:login')
@@ -23,6 +24,18 @@ def modificar_cliente_mayorista(request,pk):
             return redirect('cliente_mayorista:listado_clientes')
     form = ModificarClienteMayoristaForm(instance=cliente)
     return render(request,'cliente_mayorista/modificar_cliente_mayorista.html',{'form':form})
+
+def eliminar_cliente(request,pk):
+    cliente = get_object_or_404(ClienteMayorista, id=pk)
+    
+    if request.method == 'POST':
+        cliente.estado = False
+        cliente.save()
+        return redirect('cliente_mayorista:listado_clientes')
+    
+    else:
+        messages.error(request, "La cancelación no se pudo completar.")
+        return redirect('cliente_mayorista:listado_clientes')
 
 def listar_clientes(request):
     clientes = ClienteMayorista.objects.all()
